b06c283887003169c33b272ce4a304621252e4ea
Brian Bassett <brianbassett@brians-air.mynetworksettings.com>
2023-11-25 20:12:32 -0500

    fix mobile route

--- app/components/navbar/navbar.jsx
+++ app/components/navbar/navbar.jsx
@@ -64,7 +64,6 @@ const Navbar = () => {
             href="/resume"
             target="_blank"
             className="block text-blue-600 mb-2"
-            download={true}
           >
             Resume
           </a>
